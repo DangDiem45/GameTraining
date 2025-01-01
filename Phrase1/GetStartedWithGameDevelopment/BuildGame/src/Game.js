2af80ca2e47debb9fd8d6872958ccf220d3006ed
DangDiem45 <danghuukhanh2812@gmail.com>
2025-01-01 23:49:20 +0700

    update game

--- Phrase1/GetStartedWithGameDevelopment/BuildGame/src/Game.js
+++ Phrase1/GetStartedWithGameDevelopment/BuildGame/src/Game.js
@@ -1,3 +1,14 @@
+import TileMap  from "./TileMap.js";
+
+const tileSize = 40;
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext('2d');
+const tileMap = new TileMap(tileSize);
 
+function gameLoop(){
+    tileMap.draw(ctx);
+    // console.log("game loop");
+}
+
+tileMap.setCanvasSize(canvas);
+setInterval(gameLoop, 1000/75);
